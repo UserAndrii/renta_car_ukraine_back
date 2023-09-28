9ac7260fdb3823c6a139f6ec4c510583f7e508b2
Andrii Gadar <gadar.andre@gmail.com>
2023-09-28 23:57:21 +0300

    starting the server

---
+++ server.js
@@ -0,0 +1,27 @@
+require('colors');
+
+const mongoose = require('mongoose');
+const app = require('./app');
+
+const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose.set('strictQuery', true);
+
+mongoose
+  .connect(DB_HOST)
+
+  .then(() => {
+    console.log('Database connection successful');
+    app.listen(PORT, () => {
+      console.log(
+        `Server connection on port ${PORT} successful`.bold.green.italic
+      );
+    });
+  })
+
+  .catch(e => {
+    console.log(
+      `Server not running. Error message: ${e.message}`.bold.red.italic
+    );
+    process.exit(1);
+  });
