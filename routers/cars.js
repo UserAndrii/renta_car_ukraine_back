cebfe65f3a1d4e01559cbf449a5bf8e8c6579ec1
Andrii Gadar <gadar.andre@gmail.com>
2024-02-04 15:36:06 +0200

    completed swagger documentation on cars

--- routers/cars.js
+++ routers/cars.js
@@ -78,40 +78,77 @@ router.get('/all', ctrl.getAllCars);
  *       content:
  *         application/json:
  *           schema:
- *             $ref: '../schemas/carsSchemas/rentalCarSchema'
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: string
+ *               year:
+ *                 type: number
+ *               make:
+ *                 type: string
+ *               model:
+ *                 type: string
+ *               type:
+ *                 type: string
+ *               img:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               fuelConsumption:
+ *                 type: string
+ *               engineSize:
+ *                 type: string
+ *               accessories:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *               functionalities:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *               rentalPrice:
+ *                 type: string
+ *               rentalCompany:
+ *                 type: string
+ *               address:
+ *                 type: string
+ *               rentalConditions:
+ *                 type: string
+ *               mileage:
+ *                 type: number
  *     responses:
  *       '201':
  *         description: Successful response
  *         content:
  *           application/json:
  *             example:
- *               - _id: "65b909e3a48da347ac46dcb2"
- *                 year: 2023
- *                 make: "PEUGEOT"
- *                 model: "E-3008"
- *                 type: "Electric Car"
- *                 img: "https://res.cloudinary.com/dlnbfj7kd/image/upload/v1706625505/cars/hy0jof4h4nl2fk6fdthz.png"
- *                 description: "Meet the New PEUGEOT E-3008, which opens a new level of development and a new level of emotions..."
- *                 fuelConsumption: "Dual Motor 326 л.с., 509 Нм"
- *                 engineSize: "Electric"
- *                 accessories:
- *                   - "PEUGEOT Panoramic i-Cockpit"
- *                   - "Block of touch buttons i-Toggles"
- *                   - "Leather/Alcantara combination for the seats"
- *                   - "PEUGEOT VisioPark 360"
- *                   - "Advanced Traction Control"
- *                 functionalities:
- *                   - "21-inch HD panel"
- *                   - "i-Cockpit®"
- *                   - "Focal® Premium Hi-Fi system"
- *                   - "Drive Assist Plus"
- *                   - "ACC adaptive cruise control with Stop&Go function"
- *                   - "STLA Medium from Stellantis Concern"
- *                 rentalPrice: "$100"
- *                 rentalCompany: "Peugeot Service"
- *                 address: "400 Horodozka str, Lviv, Ukraine"
- *                 rentalConditions: "Minimum age: 26\r\nValid driver's license\r\nSecurity deposit required"
- *                 mileage: 100
+ *               _id: "65b909e3a48da347ac46dcb2"
+ *               year: 2023
+ *               make: "PEUGEOT"
+ *               model: "E-3008"
+ *               type: "Electric Car"
+ *               img: "https://res.cloudinary.com/dlnbfj7kd/image/upload/v1706625505/cars/hy0jof4h4nl2fk6fdthz.png"
+ *               description: "Meet the New PEUGEOT E-3008, which opens a new level of development and a new level of emotions..."
+ *               fuelConsumption: "Dual Motor 326 л.с., 509 Нм"
+ *               engineSize: "Electric"
+ *               accessories:
+ *                 - "PEUGEOT Panoramic i-Cockpit"
+ *                 - "Block of touch buttons i-Toggles"
+ *                 - "Leather/Alcantara combination for the seats"
+ *                 - "PEUGEOT VisioPark 360"
+ *                 - "Advanced Traction Control"
+ *               functionalities:
+ *                 - "21-inch HD panel"
+ *                 - "i-Cockpit®"
+ *                 - "Focal® Premium Hi-Fi system"
+ *                 - "Drive Assist Plus"
+ *                 - "ACC adaptive cruise control with Stop&Go function"
+ *                 - "STLA Medium from Stellantis Concern"
+ *               rentalPrice: "$100"
+ *               rentalCompany: "Peugeot Service"
+ *               address: "400 Horodozka str, Lviv, Ukraine"
+ *               rentalConditions: "Minimum age: 26\r\nValid driver's license\r\nSecurity deposit required"
+ *               mileage: 100
  */
 
 router.post(
@@ -142,11 +179,44 @@ router.post(
  *       content:
  *         application/json:
  *           schema:
- *             $ref: '../schemas/carsSchemas/updateRentalCarSchema'
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: string
+ *               year:
+ *                 type: number
+ *               make:
+ *                 type: string
+ *               model:
+ *                 type: string
+ *               type:
+ *                 type: string
+ *               img:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               fuelConsumption:
+ *                 type: string
+ *               engineSize:
+ *                 type: string
+ *               accessories:
+ *                 type: string
+ *               functionalities:
+ *                 type: string
+ *               rentalPrice:
+ *                 type: string
+ *               rentalCompany:
+ *                 type: string
+ *               address:
+ *                 type: string
+ *               rentalConditions:
+ *                 type: string
+ *               mileage:
+ *                 type: number
  *     responses:
  *       '200':
  *         description: Successful response
- *           content:
+ *         content:
  *           application/json:
  *             example:
  *               - _id: "65b909e3a48da347ac46dcb2"
